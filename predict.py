88ceadd9ec142f3e321f7f40fe16a0e5a638f5d4
Jyot Makadiya <57573673+jeromepatel@users.noreply.github.com>
2022-01-31 17:47:27 +0530

    added detectron2 example baseline

--- predict.py
+++ predict.py
@@ -1,7 +1,9 @@
-from predict_random import RandomPredictor
+# from predict_random import RandomPredictor
+from predict_detectron2 import Detectron2Predictor
+
 
 # Predictor which does nothing
-random_predictor = RandomPredictor()
+# random_predictor = RandomPredictor()
 
 # mmdetection needs `models` folder to be present in your submission, check predict_mmdetection.py to learn more
 # mmdetection_predictor = MMDetectionPredictor()
@@ -11,10 +13,11 @@ random_predictor = RandomPredictor()
 
 # Your own implementation
 # my_predictor = MyPredictor()
+detectron2_predictor = Detectron2Predictor()
 
 """
 PARTICIPANT_TODO: The implementation you want to submit as your submission
 """
-submission = random_predictor
+submission = detectron2_predictor
 submission.run()
 print("Successfully generated predictions...")
