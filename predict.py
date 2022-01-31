475da21db5049e909bfe97806d616f27a5d31bc3
Jyot Makadiya <57573673+jeromepatel@users.noreply.github.com>
2022-01-31 18:09:16 +0530

    added suggested changes

--- predict.py
+++ predict.py
@@ -11,9 +11,12 @@ from predict_detectron2 import Detectron2Predictor
 # maskrcnn needs `models` folder to be present in your submission, check predict_maskrcnn.py to learn more
 # maskrcnn_predictor = MaskRCNNPredictor()
 
+#Detectron2 needs `models` folder to be present in your submission, check predict_detectron2.py to learn more
+detectron2_predictor = Detectron2Predictor()
+
 # Your own implementation
 # my_predictor = MyPredictor()
-detectron2_predictor = Detectron2Predictor()
+
 
 """
 PARTICIPANT_TODO: The implementation you want to submit as your submission
